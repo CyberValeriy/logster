dfa6de39363f3ea30c8b688a1bd56ffe858fd300
Valerii H <valeriyhrynchyshyn@gmail.com>
2026-03-09 20:58:33 +0200

    feat(): WIP

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,11 +5,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ origin: '*' });
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, transformOptions: { enableImplicitConversion: true }}));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   await app.init();
   await app.listen(process.env.PORT ?? 3000);

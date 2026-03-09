8017ce554b475ee501c6a401f1541453a2371cc4
Valerii H <valeriyhrynchyshyn@gmail.com>
2026-03-09 21:16:55 +0200

    feat(): WIP

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,23 +1,20 @@
 import { Module } from '@nestjs/common';
-import { RedisModule } from '@nestjs-modules/ioredis';
 
 import { ClickhouseModule } from './clickhouse/clickhouse.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { getEnvConfig } from './shared';
 import { UserModule } from './user/core/user.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     /* Combine into Platform module */
+    RedisModule,
     ClickhouseModule,
     MongooseModule.forRoot(getEnvConfig().mongo.url, {
       autoIndex: false,
       autoCreate: false,
-    }),
-    RedisModule.forRoot({
-      type: 'single',
-      url: getEnvConfig().redis.url,
     }),
 
     /* Business logic modules */

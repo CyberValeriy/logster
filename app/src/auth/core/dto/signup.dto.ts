bc4669cda536f5ebf78b14c23b7e91be35d7fe98
Valerii H <valeriyhrynchyshyn@gmail.com>
2026-03-17 17:33:47 +0200

    LGSTR: add swagger for auth

--- app/src/auth/core/dto/signup.dto.ts
+++ app/src/auth/core/dto/signup.dto.ts
@@ -1,14 +1,20 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty({ example: 'test@example.com', format: 'email' })
   @IsEmail()
   public email: string;
 
+  @ApiProperty({ example: 'testuser', minLength: 3, maxLength: 128 })
   @IsString()
   @MinLength(3)
+  @MaxLength(128)
   public username: string;
 
+  @ApiProperty({ example: 'testpassword', minLength: 8, maxLength: 128 })
   @IsString()
   @MinLength(8)
+  @MaxLength(128)
   public password: string;
 }
--- app/src/main.ts
+++ app/src/main.ts
@@ -3,11 +3,24 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ origin: '*' });
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .addApiKey({ type: 'apiKey', in: 'header', name: 'project-api-key' }, 'project-api-key')
+    .setTitle('Logster')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    customSiteTitle: 'Logster API Docs.',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
